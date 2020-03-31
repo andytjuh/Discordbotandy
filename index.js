e6111de9d65a77d5997fd75d4886099491628a9c
andytjuh <andytjuh@live.nl>
2020-04-01 00:31:26 +0200

    init
    init

--- index.js
+++ index.js
@@ -77,4 +77,4 @@ bot.on('message', async message => {
     Username = messageArray[1];
     message.channel.send(`Je naam is met succes veranderd geef nu de commansd ?done ${message.author}`);
 })
-bot.login(botconfig.token);
+bot.login(process.env.token);
