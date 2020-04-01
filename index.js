3c705ea4f15e7a7dcfc348d4ff712ba786962bdb
andytjuh <andytjuh@live.nl>
2020-04-01 17:03:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.on('message', async message => {
     const pattern = /(?<=<@!)\d+(?=>)/g;
     const match = username.match(pattern);
     if (!match) {
-        message.channel.send(`!rsn ${message.author}  Hier je nieuwe naam - je eigen naam                 Kom je er niet uit stuur een general even een bericht.`);
+        message.channel.send(`(Error) Ik kan je naam niet veranderen \n !rsn ${message.author}  Hier je nieuwe naam - je eigen naam \n (Kom je er niet uit stuur een general even een bericht).`);
         return;
     }
 
@@ -78,7 +78,7 @@ bot.on('message', async message => {
     naam.setNickname(showAll);
     Nickname = messageArray[2];
     Username = messageArray[1];
-        message.channel.send(`Je Naam is veranderd. GEBRUIK nu het volgende command ?done ${message.author} \n\n\n Als je rank al recruit of hoger is ignore ?done command.`);
+        message.channel.send(`Je Naam is veranderd. GEBRUIK nu het volgende command ?done ${message.author} \n\n Als je rank al recruit of hoger is ignore ?done command.`);
 
 })
 bot.login(process.env.token);
